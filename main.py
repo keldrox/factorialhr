10631afb94225bb6e16a07c9d1cc8348a7c24a3c
Jose Maria <sampedro.jm@gmail.com>
2020-06-25 07:57:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from factorial.factorialclient import FactorialClient
 from factorial.exceptions import AuthenticationTokenNotFound, ApiError, UserNotLoggedIn
-
+from datetime import date
+import calendar
+import os
+from constants import BASE_PROJECT
 
 if __name__ == '__main__':
     try:
-        client = FactorialClient.load_from_settings()
-        client.worked_day()
+        week = calendar.day_name[date.today().weekday()];
+
+        if week == "Saturday" or week == "Sunday":
+            print ("Fin de semana")
+        elif week == "Friday":
+            config = os.path.join(BASE_PROJECT, 'viernes.json')
+        else:
+            config = os.path.join(BASE_PROJECT, 'lunes_jueves.json')
+
+        client = FactorialClient.load_from_settings(config)
+        client.worked_day(date.today(), config)
     except AuthenticationTokenNotFound as err:
         print(f"Can't retrieve the login token: {err}")
     except UserNotLoggedIn as err:
